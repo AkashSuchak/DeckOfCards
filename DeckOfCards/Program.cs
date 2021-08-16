10b7c22ff55ddd19159ab9bee21271f8fde39b09
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-16 12:32:15 +0530

    feat: Display Welcome Message

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display Message
+            Console.WriteLine("Welcome to Deck Of Cards!");
+        }
+    }
+}
